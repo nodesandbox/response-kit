e0992fa9b34ed75851204d068348d7d1b0880b65
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2025-01-07 10:37:57 +0000

    update: error response modified with more impreoved handling

--- src/api/error/response.ts
+++ src/api/error/response.ts
@@ -1,30 +1,49 @@
-/* eslint-disable @typescript-eslint/no-inferrable-types */
-
-import { ErrorCodes } from './static';
-import { ErrorCode } from './types';
+import { ErrorCodes } from "./static";
+import { ErrorCode } from "./types";
 
 export class ErrorResponse extends Error {
   public statusCode: number;
   public code: string;
-  public suggestions: string[];
+  public suggestions?: string[];
   public originalError?: Error;
 
-  constructor(
-    code: string,
-    message?: string,
-    suggestions: string[] = [],
-    originalError?: Error,
-  ) {
+  constructor(options: {
+    code: string;
+    message?: string;
+    statusCode?: number;
+    suggestions?: string[];
+    originalError?: Error;
+  }) {
+    const { code, message, statusCode, suggestions, originalError } = options;
     const errorCode: ErrorCode = ErrorCodes[code] || ErrorCodes.GENERAL_ERROR;
     super(message || errorCode.message);
     this.code = errorCode.code;
-    this.statusCode = errorCode.statusCode;
+    this.statusCode = statusCode ?? errorCode.statusCode;
     this.suggestions = suggestions;
     this.originalError = originalError;
 
-    // Capture the stack trace of the original error if provided
     if (originalError) {
       this.stack = originalError.stack;
     }
+  }
+
+  public toJSON() {
+    return {
+      code: this.code,
+      message: this.message,
+      statusCode: this.statusCode,
+      ...(this.suggestions && { suggestions: this.suggestions }),
+      originalError: this.getOriginalError(),
+    };
+  }
+
+  private getOriginalError() {
+    // if (process.env.NODE_ENV === 'production') {
+    //   return undefined;
+    // }
+    return {
+      message: this.originalError?.message,
+      stack: this.originalError?.stack,
+    };
   }
 }
